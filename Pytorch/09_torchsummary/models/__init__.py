cf019192b44bfecdbe7f59e06011e180e44f3a1f
Yuki <workcomputer.com>
2023-11-07 13:22:10 +0800

    测试torchsnooper

--- Pytorch/09_torchsummary/models/__init__.py
+++
@@ -1,5 +0,0 @@
-from .inception_v4_00 import inceptionv4
-from .inceptionresnetv2_00 import inceptionresnetv2
-from .xception_00 import xception
-from .ghostnet_00 import ghostnet
-from .efficientnet_v2_00 import efficientnet_v2_s, efficientnet_v2_m, efficientnet_v2_l
---
+++ Pytorch/34_TorchSnooper/TorchSnooper.py
@@ -0,0 +1,30 @@
+import torch
+from torch import nn, Tensor
+import torchsnooper
+
+
+class Net(nn.Module):
+    def __init__(self) -> None:
+        super().__init__()
+        self.stem = nn.Sequential(
+            nn.Conv2d(3, 16, 3, padding=1),
+            nn.BatchNorm2d(16),
+            nn.ReLU(inplace=True),
+            nn.Conv2d(16, 64, 3, padding=1),
+        )
+
+    def forward(self, x: Tensor) -> Tensor:
+        x = self.stem(x)
+        return x
+
+
+@torchsnooper.snoop()
+def test():
+    x = torch.ones(1, 3, 224, 224)
+    model = Net().cuda()                    # 将模型放到显卡上
+    model.eval()
+    with torch.inference_mode():
+        y = model(x)
+    print(y.size())
+test()
+
