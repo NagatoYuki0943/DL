5ab2e579cae9c7cfddd56aed17f818ea6f6b7f98
Yuki <workcomputer.com>
2023-11-15 10:07:57 +0800

    test amd hip and cuda

--- Pytorch/test.py
+++ Pytorch/test.py
@@ -1,20 +1,15 @@
-from re import X
-
 import torch
 import torchaudio
 import torchvision
+
 
 print(torch.__version__)
 print(torchvision.__version__)
 print(torchaudio.__version__)
 
-x = torch.rand(2, 3, 4)
 
-x1, x2 = x.unbind(0)
-print(x1.shape, x2.shape)
-# [3, 4] [3, 4]
-
-
-x3, x4, x5 = x.unbind(1)
-print(x3.shape, x4.shape, x4.shape)
-# [2, 4] [2, 4] [2, 4]
+# https://www.jianshu.com/p/996951e5e9f3
+if torch.cuda.is_available() and torch.version.hip:
+    print("do something specific for HIP")
+elif torch.cuda.is_available() and torch.version.cuda:
+    print("do something specific for CUDA")
