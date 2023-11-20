30acf54a896ab3a010af657c0357363302d1ec7c
Yuki <workcomputer.com>
2023-11-20 14:19:50 +0800

    update register_buffer&register_parameter

--- Pytorch/27_register_buffer&register_parameter/register_buffer load.py
+++ Pytorch/27_register_buffer&register_parameter/register_buffer load.py
@@ -1,18 +1,18 @@
 #-------------------------------------------------------------------------------------#
 #   self.register_buffer('name', Tensor)的操作，该方法的作用是定义一组参数，
 #   该组参数的特别之处在于：模型训练时不会更新（即调用 optimizer.step() 后该组参数不会变化，
-#   只可人为地改变它们的值），但是保存模型时，该组参数又作为模型参数不可或缺的一部分被保存。
+#   只可人为地改变它们的值，但是保存模型时，该组参数又作为模型参数不可或缺的一部分被保存。
 #-------------------------------------------------------------------------------------#
 
 
 import torch
-from torch import nn as nn
+from torch import nn, Tensor
 
 
 def load_dynamic_buffer_module(model: nn.Module, state_dict: dict) -> nn.Module:
     """解决使用register_buffer保存的参数初始化形状和保存权重形状不统一的问题
 
-    refer: https://github.com/openvinotoolkit/anomalib/blob/main/anomalib/models/components/base/dynamic_module.py#L32
+    refer: https://github.com/openvinotoolkit/anomalib/blob/main/src/anomalib/models/components/base/dynamic_module.py#L32
 
     Args:
         model (nn.Module): model
@@ -22,7 +22,7 @@ def load_dynamic_buffer_module(model: nn.Module, state_dict: dict) -> nn.Module:
     for key in state_dict.keys():
         # 如果对应位置的形状不一样,就修改模型参数的形状
         if model_state_dict[key].shape != state_dict[key].shape:
-            attribute = getattr(model, key)
+            attribute: Tensor = getattr(model, key)
             attribute.resize_(state_dict[key].shape)
 
     model.load_state_dict(state_dict)
@@ -34,7 +34,7 @@ class Model(nn.Module):
         super(Model, self).__init__()
         self.fc = nn.Linear(10, 20)
         self.register_buffer('buffer', torch.Tensor())
-        self.buffer: torch.Tensor
+        self.buffer: Tensor
 
     def forward(self, x):
         x = self.fc(x)
--- Pytorch/27_register_buffer&register_parameter/register_buffer.py
+++ Pytorch/27_register_buffer&register_parameter/register_buffer.py
@@ -1,7 +1,7 @@
 #-------------------------------------------------------------------------------------#
 #   self.register_buffer('name', Tensor)的操作，该方法的作用是定义一组参数，
-#   该组参数的特别之处在于：模型训练时不会更新（即调用 optimizer.step() 后该组参数不会变化，
-#   只可人为地改变它们的值），但是保存模型时，该组参数又作为模型参数不可或缺的一部分被保存。
+#   该组参数的特别之处在于：模型训练时不会更新(即调用 optimizer.step() 后该组参数不会变化，
+#   只可人为地改变它们的值，但是保存模型时，该组参数又作为模型参数不可或缺的一部分被保存。
 #-------------------------------------------------------------------------------------#
 
 
@@ -12,19 +12,19 @@ from torch import nn as nn
 class Model(nn.Module):
     def __init__(self) -> None:
         super(Model, self).__init__()
-        # （1）常见定义模型时的操作
+        # (1) 常见定义模型时的操作
         self.fc = nn.Linear(10, 10)
 
-        # （2）使用Parameter定义一组参数
+        # (2) 使用Parameter定义一组参数
         self.se = nn.Parameter(torch.randn(1, 2))
 
-        # （3）使用形式类似的register_parameter()定义一组参数，和上面效果相同
+        # (3) 使用形式类似的 register_parameter() 定义一组参数，和上面效果相同
         self.register_parameter('param_reg', nn.Parameter(torch.randn(1, 2)))
 
-        # （4）使用register_buffer()定义一组参数
+        # (4) 使用 register_buffer() 定义一组参数,不会被梯度更新
         self.register_buffer('param_buf', torch.randn(1, 2))
 
-        # （5）按照类的属性形式定义一组变量，不会被state_dict()保存
+        # (5) 按照类的属性形式定义一组变量，不会被state_dict()保存
         self.param_attr = torch.randn(1, 2)
 
     def forward(self, x):
