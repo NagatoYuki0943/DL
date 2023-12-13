5a7bf1bd62107a54e1c61b267be7b71739a18c49
Yuki <workcomputer.com>
2023-12-13 14:56:54 +0800

    linear implement

---
+++ Pytorch/04_Linear/linear_implement.py
@@ -0,0 +1,37 @@
+import torch
+from torch import Tensor
+from torch import nn
+
+
+torch.manual_seed(0)
+
+
+class Linear(nn.Module):
+    def __init__(self, in_features, out_features, bias=True):
+        super().__init__()
+        self.weight = nn.Parameter(torch.rand(out_features, in_features))
+        self.bias = nn.Parameter(torch.rand(out_features)) if bias else None
+
+    def forward(self, x: Tensor) -> Tensor:
+        x = x @ self.weight.T
+        if self.bias is not None:
+            x += self.bias
+        return x
+
+
+x = torch.randn(1, 10)
+linear1 = Linear(10, 5, bias=True).eval()
+linear2 = nn.Linear(10, 5, bias=True).eval()
+
+print(linear1.weight.data.shape)# [5, 10]
+print(linear1.bias.data.shape)  # [5]
+print(linear2.weight.data.shape)# [5, 10]
+print(linear2.bias.data.shape)  # [5]
+
+# repleace weight and bias
+linear2.weight.data = linear1.weight.data
+linear2.bias.data = linear1.bias.data
+
+with torch.inference_mode():
+    print(linear1(x))   # [[-0.5692,  0.1194,  0.2910, -0.3820,  0.0709]]
+    print(linear2(x))   # [[-0.5692,  0.1194,  0.2910, -0.3820,  0.0709]]
